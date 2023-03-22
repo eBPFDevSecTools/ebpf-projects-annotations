82c2d29a14aea6dd4d3121c33791aebcde0e1986
Sayandeep Sen <sayandes@in.ibm.com>
2023-03-22 08:13:48 +0000

    comment extractor code

---
+++ run4.sh
@@ -0,0 +1,21 @@
+#bcc
+python3 src/utils/comment_extractor.py -s  ./human_annotations/human_commented_bcc/ -d ./op/bcc/commented_bcc/bcc.db_comments.db
+
+cp op/bcc/commented_bcc/bcc.db_comments.db ./repo_db/bcc_annotated.db
+
+#mptm
+python3 src/utils/comment_extractor.py -s  ./human_annotations/human_commented_xdp-mptm-main/ -d ./op/xdp-mptm-main/commented_xdp-mptm-main/xdp-mptm-main.db_comments.db
+
+cp ./op/xdp-mptm-main/commented_xdp-mptm-main/xdp-mptm-main.db_comments.db ./repo_db/xdp-mptm-main_annotated.db
+
+#rate-limiter
+python3 src/utils/comment_extractor.py -s  ./human_annotations/human_commented_ebpf-ratelimiter-main -d ./op/ebpf-ratelimiter-main/commented_ebpf-ratelimiter-main/ebpf-ratelimiter-main.db_comments.db
+
+cp ./op/ebpf-ratelimiter-main/commented_ebpf-ratelimiter-main/ebpf-ratelimiter-main.db_comments.db ./repo_db/ebpf-ratelimiter-main_annotated.db
+
+#bpf-filter
+python3 src/utils/comment_extractor.py -s ./human_annotations/human_commented_bpf-filter-master/ -d  ./op/bpf-filter-master/commented_bpf-filter-master/bpf-filter-master.db_comments.db
+
+cp ./op/bpf-filter-master/commented_bpf-filter-master/bpf-filter-master.db_comments.db ./repo_db/bpf-filter-master_annotated.db
+
+

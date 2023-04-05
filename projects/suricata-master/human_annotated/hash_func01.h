ef0f572495b946bb6c13c858ecd3a6b18632834e
Aaseesh Rallapalli <86010924+aashu2303@users.noreply.github.com>
2023-04-05 15:20:38 +0530

    Update hash_func01.h

--- projects/suricata-master/human_annotated/hash_func01.h
+++ projects/suricata-master/human_annotated/hash_func01.h
@@ -123,10 +123,10 @@
   "call_depth": -1,
   "humanFuncDescription": [
     {
-      "description": "",
-      "author": "",
-      "authorEmail": "",
-      "date": ""
+      "description": "This function is the implementation of the SuperFastHash algorithm by Paul Hsieh of MIT. This function returns the hash of length len, for the input data based on the random state initval.",
+      "author": "R V B R N Aaseesh",
+      "authorEmail": "ee20btech11060@iith.ac.in",
+      "date": "2023-04-05"
     }
   ],
   "AI_func_description": [

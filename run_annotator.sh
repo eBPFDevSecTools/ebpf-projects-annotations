e0c8fe3e433ec178103c7b64c39e7015439c1d87
Sayandeep Sen <sayandes@in.ibm.com>
2023-04-08 06:13:05 +0000

    run comment extractor on ingress-node-firewall
    Signed-off-by: Sayandeep Sen <sayandes@in.ibm.com>

--- run_annotator.sh
+++ run_annotator.sh
@@ -22,9 +22,16 @@ python3 src/annotator.py -o projects/netobserv-bpf-main/intermediate/txl_netobse
 #suricata
 python3 src/annotator.py -o projects/suricata-master/intermediate/txl_suricata-master -s projects/suricata-master/original_source -c projects/suricata-master/human_annotated/ -t projects/suricata-master/intermediate/suricata-master.function_file_list.json -u projects/suricata-master/intermediate/suricata-master.struct_file_list.json -p suricata-master
 
+#loxilb
+python3 src/annotator.py -o projects/loxilb-ebpf/intermediate/txl_loxilb-ebpf -s projects/loxilb-ebpf/original_source -c projects/loxilb-ebpf/human_annotated/ -t projects/loxilb-ebpf/intermediate/loxilb-ebpf.function_file_list.json -u projects/loxilb-ebpf/intermediate/loxilb-ebpf.struct_file_list.json -p loxilb-ebpf
+
+#ingress-node-firewall
+python3 src/annotator.py -o projects/ingress-node-firewall-master/intermediate/txl_ingress-node-firewall-master -s projects/ingress-node-firewall-master/original_source -c projects/ingress-node-firewall-master/human_annotated -t projects/ingress-node-firewall-master/intermediate/ingress-node-firewall-master.function_file_list.json -u projects/ingress-node-firewall-master/intermediate/ingress-node-firewall-master.struct_file_list.json -p ingress-node-firewall-master
+
+
 #python3 src/annotator.py -o op/kpng-master/txl_kpng-master -s examples/kpng-master/backends/ebpf/bpf -c op/kpng-master/commented_kpng-master -t op/kpng-master/kpng-master.function_file_list.json -u op/kpng-master/kpng-master.struct_file_list.json -p kpng-master
 
-#python3 src/annotator.py -o op/ingress-node-firewall-master/txl_ingress-node-firewall-master -s examples/ingress-node-firewall-master/bpf -c op/ingress-node-firewall-master/commented_ingress-node-firewall-master -t op/ingress-node-firewall-master/ingress-node-firewall-master.function_file_list.json -u op/ingress-node-firewall-master/ingress-node-firewall-master.struct_file_list.json -p ingress-node-firewall-master
+
 
 
 #python3 src/annotator.py -o op/vpf-ebpf-src/txl_vpf-ebpf-src -s examples/vpf-ebpf-src -c op/vpf-ebpf-src/commented_vpf-ebpf-src -t op/vpf-ebpf-src/vpf-ebpf-src.function_file_list.json -u op/vpf-ebpf-src/vpf-ebpf-src.struct_file_list.json -p vpf-ebpf-src
